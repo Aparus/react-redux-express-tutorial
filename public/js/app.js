a47a4a8b88cce588e80e9e9db3f59924463227e1
Rustam Apaev <rapaev@abisoft.biz>
2017-11-09 18:50:49 +0300

    12 lesson ended (all course)

--- public/js/app.js
+++ public/js/app.js
@@ -4,14 +4,30 @@ import Counter from './Counter'
 import { Provider } from 'react-redux'
 import { createStore, combineReducers } from 'redux'
 import counterApp from './reducers'
-import { Router, Route, browserHistory, IndexRoute } from 'react-redux'
+import MainComponent from './MainComponent'
+import AnotherComponent from './AnotherComponent'
+import { Router, Route, browserHistory, IndexRoute } from 'react-router'
 import { syncHistoryWithStore, routerReducer } from 'react-router-redux'
 
-let store = createStore(counterApp)
+
+const store = createStore(
+    combineReducers({
+        global: counterApp,
+        routing: routerReducer
+    }), 
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+const history = syncHistoryWithStore(browserHistory, store)
 
 render(
     <Provider store={store}>
-        <Counter />
+        <Router history={history}>
+            <Route path="/" component={MainComponent}>
+                <IndexRoute component={Counter} />
+                <Route path="test" component={AnotherComponent} />
+            </Route>
+        </Router>
     </Provider>, 
     document.getElementById('app')
 )
